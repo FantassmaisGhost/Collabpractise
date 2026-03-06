d31f47daa1e1c85877df2bb1b9c95046e30064d1
Lungani Shezi <dyingmountain58@gmail.com>
2026-03-06 21:04:08 +0200

    final commit. I guess.

--- script.js
+++ script.js
@@ -5,6 +5,43 @@ const Country = async () => {
         const input = document.getElementById("country-input").value.toLowerCase();
         const url = `https://restcountries.com/v3.1/name/${input}`;
         const response = await fetch(url);
+
+        if (!response.ok){
+            throw new Error("Country not found.")
+        }
+
+        const search = await response.json();
+
+        console.log(search);
+
+        document.getElementById('country-info').innerHTML = 
+        `<section>
+        <h2>${search[0].name.common}</h2> 
+        <p><strong>Capital:</strong> ${search[0].capital[0]}</p>
+        <p><strong>Population:</strong> ${search[0].population.toLocaleString()}</p>
+        <p><strong>Region:</strong> ${search[0].region}</p>
+        <img src="${search[0].flags.svg}" alt="${search[0].name.common} flag" width="150">
+        </section>`;
+
+        const borders = search[0].borders;
+
+        for (let code of borders){
+            const bordresp = await fetch(`https://restcountries.com/v3.1/alpha/${code}`);
+
+            if (!bordresp.ok){
+            throw new Error("Country not found.")
+            }
+
+            const data = await bordresp.json();
+
+            console.log(data);
+
+            document.getElementById('bordering-countries').innerHTML += 
+            `<p>${data[0].name.common}</p> 
+            <img src="${data[0].flags.svg}" width = "100">`;
+        }
+
+
     }
     catch{
 
@@ -13,3 +50,5 @@ const Country = async () => {
 
     }
 }
+
+btnsearch.addEventListener("click",Country);
